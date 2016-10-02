b6608c96b185f43e4eaa694a4d0208803e796802
Arnold Lei <arnold.lei88@gmail.com>
2016-10-02 17:57:26 -0400

    got the db to work!

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,26 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-
+import ReactDOM from 'react-dom';
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+    handleSubmit(event) {
+      event.preventDefault();
+
+      // Find the text field via the React ref
+      const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+      Tasks.insert({
+        text,
+        createdAt: new Date(), // current time
+      });
+
+      // Clear form
+      ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
