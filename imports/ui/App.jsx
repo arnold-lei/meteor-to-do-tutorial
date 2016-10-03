cc2470ba480301fd5c4f81f6653c40ec7c07d80b
Arnold Lei <arnold.lei88@gmail.com>
2016-10-03 19:59:19 -0400

    got the tasks to work

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,13 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        hideCompleted: false,
+      };
+    }
     handleSubmit(event) {
       event.preventDefault();
 
@@ -21,8 +28,18 @@ class App extends Component {
       // Clear form
       ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
+    toggleHideCompleted() {
+      this.setState({
+        hideCompleted: !this.state.hideCompleted,
+      });
+    }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+      let filteredTasks = this.props.tasks;
+      if (this.state.hideCompleted) {
+        filteredTasks = filteredTasks.filter(task => !task.checked);
+      }
+      return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -32,6 +49,16 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
@@ -57,5 +84,6 @@ App.propTypes = {
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, {sort: {createdAt:-1}}).fetch(),
+    incompleteCount: PropTypes.number.isRequired,
   };
 }, App);
